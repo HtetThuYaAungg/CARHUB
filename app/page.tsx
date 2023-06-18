d1dfac711d9398a0ede10ead9721f10674ef0e8b
HTETTHU <112954138+HtetThuYaAungg@users.noreply.github.com>
2023-06-18 22:32:39 +0630

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,107 @@
+"use client"
+
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from '@/components'
+import { fuels, yearsOfProduction } from '@/constants';
+import { CarState } from '@/types';
+import { fetchCars } from '@/utils'
+import Image from 'next/image'
+import { useEffect, useState } from 'react';
+
+export default function Home() {
+
+  const [allCars, setAllCars] = useState<CarState>([]);
+  const [loading, setLoading] = useState(false);
+  const [manufacturer, setManufacturer] = useState("");
+  const [model, setModel] = useState("");
+  const [fuel, setFuel] = useState("");
+  const [year, setYear] = useState(2022);
+  const [limit, setLimit] = useState(10);
+
+  const getCars = async () => {
+
+    setLoading(true);
+    try {
+
+      const result = await fetchCars({
+        manufacturer: manufacturer || '',
+        year: year || 2022,
+        fuel: fuel || '',
+        model: model || '',
+        limit: limit || 10,
+      })
+  
+      setAllCars(result);
+      console.log({result})
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getCars()
+    console.log(fuel,year,limit,model,manufacturer)
+  },[fuel,year,limit,model,manufacturer])
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+      
+      <div className='mt-12 padding-x padding-y max-width' id="dicover">
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalog</h1>
+          <p>Explore the cars you might</p>
+        </div>
+        <div className='home__filters'>
+          <SearchBar setManufacturer={setManufacturer} setModel= {setModel} />
+          <div className='home__filter-container'>
+            <CustomFilter title="fuels" options={fuels} setFilter= {setFuel} />
+            <CustomFilter title="year" options={yearsOfProduction} setFilter= {setYear}  />
+          </div>
+        </div>
+
+        {allCars.length > 0 ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car,index) => (
+                <CarCard key={`car-${index}`} car={car} />
+              ))}
+            </div>
+
+            {loading && (
+              <div className='mt-16 w-full flex-center'>
+                <Image
+                  src="./loader.svg"
+                  alt='loader'
+                  width={50}
+                  height={50}
+                  className=' object-contain'
+                />
+              </div>
+            ) }
+
+            <ShowMore
+              pageNumber={limit / 10}
+              isNext={limit > allCars.length}
+              setLimit = {setLimit}
+            />
+          </section>
+        ) : (
+            !loading && (
+            <div className='home__error-container'>
+
+              <h2 className=' text-black text-xl font-bold'>Oops! no result</h2>
+              <p>{allCars?.message}</p>
+              
+              </div>
+            )
+        )}
+
+      </div>
+    </main>
+  )
+}
+ 
---
+++ components/Hero.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import React from 'react'
+import CustomButton from './CustomButton'
+import Image from 'next/image'
+
+const Hero = () => {
+
+  const handleScroll = () => {
+    
+  }
+
+  return (
+    <div className='hero'>
+      <div className=' flex-1 pt-32 padding-x'>
+        <h1 className='hero__title'>Find, Book or Rent a car - quickly and easily!</h1>
+        <p className='hero__subtitle'>Streamline your car rental experience wiht our efforless booking process.</p>
+        <CustomButton
+          title="Explore Cars"
+          containerStyles=" bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className='hero__image-container'>
+        <div className='hero__image'>
+          <Image
+            src='/hero.png'
+            alt="hero"
+            
+            fill className="object-contain"
+          />
+          <div className='hero__image-overlay'>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
+
